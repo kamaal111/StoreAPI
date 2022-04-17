7a783d00d2b10f08f58e4c0e0a27e78a8dd7a0ce
Kamaal <kamaal.f1@gmail.com>
2022-04-17 16:51:08 +0200

    Making jwt with key id

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello")
+import jwt
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    key_id = environ.get("KEY_ID")
+    if not key_id:
+        raise Exception("no key id provided")
+
+    payload_data = {"alg": "ES256", "kid": key_id, "typ": "JWT"}
+    my_secret = "my_super_secret"
+    token = jwt.encode(payload=payload_data, key=my_secret)
+
+    print(token)
+
+
+if __name__ == "__main__":
+    main()
